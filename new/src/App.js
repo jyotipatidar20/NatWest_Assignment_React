c9fffad214854194fc42865ae9fb282eb810bc9b
Jyoti Patidar <jyotipatidar20@navgurukul.org>
2022-12-13 14:57:58 +0530

    first commit

--- new/src/App.js
+++ new/src/App.js
@@ -1,4 +1,21 @@
+import Body from './component/Body';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import './App.css';
 
+export default function Signup(){
+    return(
+        <div  id="bigbox">
+        <div id="mainbox">
+             <Header/>
+             <Footer/>
+            <Body/>
+            
+
+        </div>
+        </div>
+    )
+}
 
 
 
